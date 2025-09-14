f082d2ac824065461ef01bab75b86f48962d8287
jesse <Jessedekoe03@gmail.com>
2025-09-14 11:14:19 +0200

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import { Article } from "./http/ArticleType";
-import useMobileView from "./hooks/useMobileView";
 
 function App() {
-  const isMobile = useMobileView();
-
   const refreshPage = () => {
     window.location.reload();
   };
-
   const [articles, setArticles] = useState<Article[]>([]);
 
   useEffect(() => {
@@ -31,52 +27,31 @@ function App() {
 
   return (
     <div className={"App"}>
-      <h1 onClick={refreshPage}>News&nbsp;App</h1>
-      <div className={isMobile ? "content-mobile" : "content"}>
+      <h1 onClick={refreshPage}>Lazy&thinsp;News&nbsp;App</h1>
+      <div className="content">
         {articles.length > 0 ? (
           articles.map((a, i) => (
             <div className={"content-item"}>
-              <div
-                className={isMobile ? "content-title-mobile" : "content-title"}
-                key={i}
-              >
+              <div className={"content-title"} key={i}>
                 {a.title}
               </div>
-              <div
-                className={
-                  isMobile
-                    ? "content-description-mobile"
-                    : "content-description"
-                }
-              >
+              <div className={"content-description"}>
                 {a.description}&nbsp;
                 <a href={`/article/${a.id}`} className={"read-more"}>
                   [&thinsp;&gt;&thinsp;]
                 </a>
               </div>
-              <div
-                className={
-                  isMobile ? "content-source-mobile" : "content-source"
-                }
-              >
+              <div className={"content-source"}>
                 <b>Source:&nbsp;</b>{" "}
-                <a
-                  href={a.source.url}
-                  target={"_blank"}
-                  rel="noreferrer noopener"
-                >
+                <a href={a.source.url} target={"_blank"}>
                   {a.source.url}
                 </a>
                 &nbsp;
                 <b>Name:&nbsp;</b>
                 {a.source.name}
               </div>
-              <div
-                className={
-                  isMobile ? "content-source-mobile" : "content-source"
-                }
-              >
-                <a href={a.url} target={"_blank"} rel="noreferrer noopener">
+              <div className={"content-source"}>
+                <a href={a.url} target={"_blank"}>
                   Read original article
                 </a>
               </div>
