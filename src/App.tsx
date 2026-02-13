9c22945951480e2146cff8598fd24f71256bbdee
Jesse <Jessedekoe03@gmail.com>
2026-02-13 21:18:30 +0100

    Valentines Gift Girlfriend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import "./index.css";
 import GeneralNews from "./pages/GeneralNews";
 import RSSFeeds from "./pages/RSSFeeds";
+import VibeCodeValentine from "./VibeCodeValentine";
 
 function App() {
   const refreshPage = () => {
     window.location.reload();
   };
 
+  const isValentine = window.location.pathname === "/valentine";
+
+  if (isValentine) {
+    return <VibeCodeValentine />;
+  }
+
   return (
-    <div className={"App"}>
+    <div className="App">
       <h1 onClick={refreshPage}>Lazy&thinsp;News&nbsp;App</h1>
       <div>
         <button type="button">Click for More News</button>
